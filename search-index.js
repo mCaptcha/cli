c6062a6294e924ac243bbb8f0339812375983364
realaravinth <realaravinth@batsense.net>
2021-03-09 10:24:38 +0000

    Deploying to gh-pages from  @ 98b5826049b663bd3cd0a9ee9000d8841cc9decc 🚀

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"mcaptcha_cli":{"doc":"","i":[[5,"main","mcaptcha_cli","",null,[[]]]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["mcaptcha_cli"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
