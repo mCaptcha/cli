cac640c07d47564bccd12f237b7b3b163471ff70
Aravinth Manivannan <realaravinth@batsense.net>
2023-06-15 13:03:56 +0530

    chore: update deps and bumb versino to 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
-use clap::{App, Arg};
+/*
+ * mCaptcha - A proof of work based DoS protection system
+ * Copyright © 2023 Aravinth Manivannan <realravinth@batsense.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+use clap::*;
 use pow_sha256::ConfigBuilder;
 
+
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Salt with which PoW should be computed
+    #[arg(short, long)]
+    salt: String,
+
+    /// Phrase over which PoW should be computed
+    #[arg(short, long)]
+    phrase: String,
+
+
+    /// Difficulty Factor
+    #[arg(short, long)]
+    difficulty_factor: u32,
+}
+
 fn main() {
-    let matches = App::new("mCaptcha PoW CLI")
-        .version("0.1.0")
-        .author("Aravinth Manivannan <realaravinth@batsense.net>")
-        .about("Generates PoW for mCaptcha")
-        .arg(
-            Arg::with_name("salt")
-                .short("-s")
-                .long("--salt")
-                .value_name("STRING")
-                .help("Salt with which PoW should be computed")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("phrase")
-                .short("-p")
-                .long("--phrase")
-                .value_name("STRING")
-                .help("Phrase over which PoW should be computed")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("difficulty_factor")
-                .short("-d")
-                .long("--difficulty")
-                .value_name("INTEGER")
-                .help("Difficulty factor")
-                .takes_value(true),
-        )
-        .get_matches();
-    let phrase = matches.value_of("phrase").unwrap();
-    let salt = matches.value_of("salt").unwrap();
-    let difficulty_factor: u32 = matches
-        .value_of("difficulty_factor")
-        .unwrap()
-        .parse()
-        .expect("Please enter an integer for difficulty");
+   let matches = Args::parse();
+    let phrase = matches.phrase;
+    let salt = matches.salt;
+    let difficulty_factor = matches.difficulty_factor;
 
     let config = ConfigBuilder::default().salt(salt.into()).build().unwrap();
     let work = config.prove_work(&phrase, difficulty_factor).unwrap();
