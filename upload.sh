b4106f07183471c187ebd7d47e3eaa2e13c1ea9c
Aravinth Manivannan <realaravinth@batsense.net>
2023-10-23 18:58:46 +0530

    feat: upload to dl.mcaptcha.org

---
+++ upload.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xEeuo  pipefail
+readonly UPLOAD_DIR="cli/master"
+readonly DUMBSERVE_HOST="https://$DUMBSERVE_USERNAME:$DUMBSERVE_PASSWORD@dl.mcaptcha.org"
+
+
+curl --location --request DELETE "$DUMBSERVE_HOST/api/v1/files/delete" \
+	--header 'Content-Type: application/json' \
+	--data-raw "{
+		\"path\": \"$UPLOAD_DIR\"
+	}"
+
+
+pushd dist/
+for file in *
+do
+	curl -v \
+		-F upload=@$file  \
+		"$DUMBSERVE_HOST/api/v1/files/upload?path=$UPLOAD_DIR"
+done
+popd
